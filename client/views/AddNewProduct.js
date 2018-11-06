20229a92d6daf8fb44207b7f74fe35cd3df1999a
Diana Voyanets <diana.voyanets@gmail.com>
2018-11-06 15:09:56 +0300

    added "Add new product" form

--- client/views/AddNewProduct.js
+++ client/views/AddNewProduct.js
@@ -3,8 +3,46 @@ import {JetView} from "webix-jet";
 export default class AddNewProduct extends JetView {
     config() {
         return {
-            view: "template",
-            template: "Add new product"
+            view: "form",
+            elements: [
+                {view: "spacer",height: 100},
+                {
+                    cols: [
+                        {view: "spacer",width: 350},
+                        {view: "text",label: "Name:",required:true},
+                        {view: "spacer",width: 350}
+                    ]
+                },
+                {
+                    cols: [
+                        {view: "spacer",width: 350},
+                        {view: "text",label: "Price:",required:true},
+                        {view: "spacer",width: 350}
+                    ]
+                },
+                {
+                    cols: [
+                        {view: "spacer",width: 350},
+                        {view:"uploader",value:"Add picture"},
+                        {view: "spacer",width: 350}
+                    ]
+                },
+                {
+                    cols: [
+                        {view: "spacer",width: 350},
+                        {view: "textarea"},
+                        {view: "spacer",width: 350}
+                    ]
+                },
+                {
+                    cols: [
+                        {view: "spacer",width: 850},
+                        {view: "button",value: "Add new product",width: 300},
+                        {view: "spacer"},
+                    ]
+                },
+                {view: "spacer"}
+            ]
         }
     }
 }
