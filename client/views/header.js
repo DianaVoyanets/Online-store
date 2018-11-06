3dc22737670597c9d3379e18c6ae19009d9fb0fb
Diana Voyanets <diana.voyanets@gmail.com>
2018-11-06 15:13:53 +0300

    added user name label

--- client/views/header.js
+++ client/views/header.js
@@ -6,13 +6,30 @@ export default  class Header extends JetView {
             view: "toolbar",
             elements: [
                 {view: "label",label: "Shop"},
-                {view: "label", label: "Hi,varyas!"},
-                {view: "button",value: "History",width: 200},
-                {view: "button",value: "Bag",width: 200},
+                {view: "label", template: `<b>${this.app.getService('user').getUser().login} shop</b>`},
+                {view: "button",localId: 'history:button',value: "History",width: 200,click: () => this.show('historyDatatable')},
+                {view: "button",localId: "bag:button",value: "Bag",width: 200,click: () => this.show('productsInBag')},
                 {view: "button",value: "Logout",width: 200,
                     click: () => this.show('/logout')
                 },
             ]
         }
+    }
+
+    init() {
+        this.usersButtonShow();
+        if(this.app.getService('user').getUser().login === 'admin') {
+           this.usersButtonHide();
+        }
+    }
+
+    usersButtonShow() {
+        this.$$("history:button").show();
+        this.$$("bag:button").show();
+    }
+
+    usersButtonHide() {
+        this.$$("history:button").hide();
+        this.$$("bag:button").hide();
     }
 }
