bd4056ff1b3c05aed13c6d1e8d454b16b963449c
Diana Voyanets <diana.voyanets@gmail.com>
2018-11-19 18:35:36 +0300

    added function: "add new product"

--- client/views/AddNewProduct.js
+++ client/views/AddNewProduct.js
@@ -1,5 +1,7 @@
 import {JetView} from "webix-jet";
-
+import {Products} from '../models/ProductsCollection';
+import {PhoneBrand} from '../models/phoneBrand';
+ 
 export default class AddNewProduct extends JetView {
     config() {
         return {
@@ -9,40 +11,48 @@ export default class AddNewProduct extends JetView {
                 {
                     cols: [
                         {view: "spacer",width: 350},
-                        {view: "text",label: "Name:",required:true},
+                        {view: "text",label: "Name:", labelWidth: 100,name: 'productName', required:true},
                         {view: "spacer",width: 350}
                     ]
                 },
                 {
                     cols: [
                         {view: "spacer",width: 350},
-                        {view: "text",label: "Price:",required:true},
+                        {view: "text",label: "Price:", labelWidth: 100,name: 'price',required:true},
                         {view: "spacer",width: 350}
                     ]
                 },
                 {
                     cols: [
                         {view: "spacer",width: 350},
-                        {view:"uploader",value:"Add picture"},
+                        // {view:"uploader",value:"Add picture"},
+                        {view: "text", label: "Image", labelWidth: 100,name: 'image',required: true},
                         {view: "spacer",width: 350}
                     ]
                 },
                 {
                     cols: [
                         {view: "spacer",width: 350},
-                        {view: "textarea"},
+                        // {view:"uploader",value:"Add picture"},
+                        {view: "combo", labelWidth: 100,localId: "test",label: "Phone brand",options:{body: {template: '#phone_brand#',data: PhoneBrand}}},
                         {view: "spacer",width: 350}
                     ]
                 },
                 {
                     cols: [
                         {view: "spacer",width: 850},
-                        {view: "button",value: "Add new product",width: 300},
+                        {view: "button",value: "Add new product",width: 300,click: () => this.addNewProduct()},
                         {view: "spacer"},
                     ]
                 },
                 {view: "spacer"}
             ]
         }
+    }
+
+    addNewProduct() {
+        let itemFromFormInput = this.getRoot().getValues();
+        Products.add(itemFromFormInput);
+        this.getRoot().clear();
     }
 }
