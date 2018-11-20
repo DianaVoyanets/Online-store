4a4a474381c08196f7766380fffb254f8fbc101f
diana <diana.voyanets@gmail.com>
2018-11-21 02:01:27 +0300

    added DataDriver into tree file

--- client/views/tree.js
+++ client/views/tree.js
@@ -1,5 +1,38 @@
 import {JetView} from "webix-jet";
-import {PhoneBrand} from "../models/phoneBrand";
+webix.DataDriver.plainjs = webix.extend({
+    arr2hash: function (data) {
+        var hash = {};
+        for (var i = 0; i < data.length; i++) {
+            var parentId = data[i].parent_id;
+            if(!parentId) {
+                parentId = 0;
+            }
+            if (!hash[parentId]) {
+                if(!parentId) {
+                    hash[0] = [];
+                    hash[0].push(data[i]); 
+                } else {
+                    hash[parentId] = [];
+                    hash[parentId].push(data[i]);
+                }
+            }
+        }
+        return hash;
+    },
+    hash2tree: function (hash, level) {
+        var top = hash[level];
+        for (var i = 0; i < top.length; i++) {
+            var branch = top[i].id;
+            if (hash[branch])
+                top[i].data = this.hash2tree(hash, branch);
+        }
+        return top;
+    },
+    getRecords: function (data, id) {
+        var hash = this.arr2hash(data);
+        return this.hash2tree(hash, 0);
+    }
+}, webix.DataDriver.json);
 
 export default class Tree extends JetView {
     config() {
@@ -8,15 +41,17 @@ export default class Tree extends JetView {
                 { 
                     view:"tree", 
                     width: 300,
-                    template:"{common.icon()}{common.folder()}#categoryName#",
-                    url: "/server/category"
+                    datatype: "plainjs",
+                    data: [
+                        {id: 1,value: 'Phone'},
+                        {id: 2,parent_id: 1,value: 'test'}
+                    ]
                 }
                 ]
             }
         }
 
     init() {
-        
     }
 
 
