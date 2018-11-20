c45382910cc825d94eb6ce6565eb097d13b1ed9c
Diana Voyanets <diana.voyanets@gmail.com>
2018-11-20 16:26:56 +0300

    refactored productInformationPopup file

--- client/views/productInformationPopup.js
+++ client/views/productInformationPopup.js
@@ -1,11 +1,12 @@
 import {JetView} from "webix-jet";
+import {Products} from "../models/ProductsCollection";
 
 export default class productInfprmationPopup extends JetView {
     config() {
         return {
             view: "popup",
             width: 500,
-            height: 400,
+            height: 350,
             modal: true,
             position: "center", 
             body:{
@@ -14,8 +15,11 @@ export default class productInfprmationPopup extends JetView {
                         view: "toolbar",
                         css: "window_toolbar",
                         cols: [
-                            {view:"label", localId: "product:name:label",label: "#name#", width: 450,align: "center"},
-                            {template: "<span class='close_window'>×</span>", 
+                            {view:"label",localId: "product:name:label",label: "#name#", width: 450,align: "center"},
+                            {
+                                localId: 'product:information:toolbar',
+                                css: 'close_window_icon',
+                                template: `<span class='close_window'>×</span>`, 
                                 onClick: {
                                     "close_window": () => this.hide()
                                 }
@@ -23,13 +27,20 @@ export default class productInfprmationPopup extends JetView {
                         ]
                     },
                     {
-                        localId: "product:information:template", 
-                        template: (obj) => {
-                            return (
-                                `<span>Name: ${obj.name}</span><br>
-                                <span>Price: ${obj.price}</span><br>
-                                <span>Rating: ${obj.rating}</span>` 
-                            )
+                        localId: "product:information:layout",
+                        template: (obj) => `<div class="phone_information">
+                                                <div style="margin-left:0;margin-top: 50px">${obj.image}</div>
+                                                <div style="margin-left: 200px;margin-bottom: 30px"><b>Name:</b> ${obj.productName}</div>
+                                                <div style="margin-left: 200px;margin-bottom: 30px"><b>Price:</b> ${obj.price}</div>
+                                                <div style="margin-left: 200px"><b>Rating:</b> ${obj.rating} <span style="font-size: 19px"><i class="mdi mdi-star-half"></i></span></div>
+                                            </div>`,
+                        onClick: {
+                            'mdi-star-half': () => {
+                                let selecteItemId = this.datatable.getSelectedItem().id;
+                                let dataFromProductsCollection = Products.getItem(selecteItemId);
+                                dataFromProductsCollection.rating++;
+                                Products.updateItem(this.datatable.getSelectedItem().id,dataFromProductsCollection);
+                            }
                         }
                     }
                 ]
@@ -37,10 +48,19 @@ export default class productInfprmationPopup extends JetView {
         }
     }
 
+    init() {
+        Products.attachEvent('onDataUpdate', () => this.$$('product:information:layout').refresh());
+    }
+
     show(options) {
-        this.$$("product:name:label").setValue(options.row.name);
-        this.$$("product:information:template").setValues(options.row);
+        this.datatable = options.getDatatable;
+        this.$$("product:name:label").setValue(options.row.productName);
+        this.$$("product:information:layout").setValues(options.row);
         this.getRoot().show();
+    }
+
+    getDataFromDatatable() {
+        this.datatable
     }
 
     hide() {
