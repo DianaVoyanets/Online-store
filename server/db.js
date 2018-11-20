f3cfbf32acabc93d36280fd3f6fdaccd3e1e0aa7
diana <diana.voyanets@gmail.com>
2018-11-21 02:01:51 +0300

    fixed db.js file

--- server/db.js
+++ server/db.js
@@ -28,76 +28,38 @@ const Products = sequelize.define('products',{
     rating: Sequelize.INTEGER
 });
 
-const phoneBrand = sequelize.define('phoneBrand',{
-    phone_brand: Sequelize.STRING
-});
-
 const categoryOfProducts = sequelize.define('categoryOfProduct',{
     categoryName: Sequelize.STRING
 });
 
-    /*PHONE BRAND CREATE*/
+const phoneBrand = sequelize.define('phoneBrand',{
+    phone_brand: Sequelize.STRING
+});
 
-    phoneBrand.findAll({ where: phoneBrand })
-        .then((phoneBrands) => {
-            if (phoneBrands.length > 0) {
-                return;
-            } else {
-                phoneBrand.create({ phone_brand: 'Samsung'});
-                phoneBrand.create({ phone_brand: 'Xiaomi'});
-                phoneBrand.create({ phone_brand: 'Apple'});
-                phoneBrand.create({ phone_brand: 'Huawei'});
-                phoneBrand.create({ phone_brand: 'Honor'});
-                phoneBrand.create({ phone_brand: 'Nokia'});
-                phoneBrand.create({ phone_brand: 'MEIZU'});
-                phoneBrand.create({ phone_brand: 'OnePlus'});
-                phoneBrand.create({ phone_brand: 'ASUS'});
-            }
-    });
+// phoneBrand.sync({force: true}).then(() => 
+    phoneBrand.create({ phone_brand: 'Xiaomi'}),
+// );
 
-    /*CATEGORY CREATE*/
+// categoryOfProducts.sync({force: true}).then(() => {
+categoryOfProducts.create({ categoryName: 'Phones'});
+// });
 
-    categoryOfProducts
-        .findAll({ where: categoryOfProducts })
-        .then((category) => {
-            if ( category.length > 0) {
-                return;
-            } else {
-                categoryOfProducts.create({ categoryName: 'Phones'});
-            }
-    });
+Role.sync({force: true}).then(() => 
+   Role.create({ roleName: 'admin'}),
+   Role.create({ roleName: 'user'})
+);
 
-    Role
-        .findAll({ where: Role })
-        .then((roles) => {
-            if (roles.length > 0) {
-                return;
-            } else {
-                Role.create({ roleName: 'user'});
-                Role.create({ roleName: 'admin'})
-            }
-    });
+Products.sync({force: true}).then(() => {
+    return  Products.create({ 
+            image: 'Very beatiful picture',
+            productName: 'Samsung galaxy S6 Edge+12',
+            price: 400,
+            rating: 0
+        });
+});
 
-    /*Products CREATE*/
-
-    Products.findAll({ where: Products })
-        .then((product) => {
-            if (product.length > 0) {
-                return;
-            } else {
-                Products.create({ 
-                    image: 'Very beatiful picture',
-                    productName: 'Samsung galaxy S6 Edge+12',
-                    price: 400,
-                    rating: 0
-                });
-            }
-    });
-
-
-    User.belongsTo(Role, {as: 'role'});
-    phoneBrand.belongsTo(categoryOfProducts,{as: 'category'});
-
+User.belongsTo(Role, {as: 'role'});
+phoneBrand.belongsTo(categoryOfProducts,{as: 'category'});
 
 module.exports = {
     sequelize,User, Role, Products, phoneBrand,categoryOfProducts
